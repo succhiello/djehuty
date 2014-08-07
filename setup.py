5ec7ca3833709ce4d5a999d6fca1e7bc692c762e
kiri <satoshi_ebihara@xica.net>
2014-08-07 23:34:12 +0900

    add package classifiers.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 
 install_requires = [
-    'pyramid',
-    'cornice',
-    'cliff',
+    'pyramid==1.5.1',
+    'cornice==0.16.2',
+    'cliff==1.6.1',
 ]
 
 setup(
@@ -14,7 +14,20 @@ setup(
     description='chat bot manager',
     author='xica development team',
     author_email='info@xica.net',
-    url='http://xica-inc.com',
+    url='https://github.com/xica/thoth',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
     packages=find_packages(),
     install_requires=install_requires,
     entry_points={
