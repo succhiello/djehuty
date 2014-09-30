060f4e135d06fa24c0b4df82f4b503e0dbf5ce51
kiri <satoshi_ebihara@xica.net>
2014-09-30 23:04:53 +0900

    add greeting option for yo.

--- djehuty/command/yo.py
+++ djehuty/command/yo.py
@@ -6,5 +6,12 @@ from djehuty.command import Command
 class Yo(Command):
     '''echo yo'''
 
+    def get_parser(self, prog_name):
+        parser = Command.get_parser(self, prog_name)
+        parser.add_argument('-g', '--greeting',
+                            default='yo',
+                            help='greeting message')
+        return parser
+
     def take_action(self, parsed_args):
-        return ('@{} '.format(self.app_args.user) if self.app_args.user else '') + 'yo'
+        return ('@{} '.format(self.app_args.user) if self.app_args.user else '') + parsed_args.greeting
